8f24e3958147123ac342f43e2213e389bf4bbfda
matan2288 <60103076+matan2288@users.noreply.github.com>
2024-08-12 13:41:39 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import FeedbackList from "./Components/FeebackList.svelte";
+  import FeedbackStats from './Components/FeedbackStats.svelte'
+  import FeedbackForm from "./Components/FeedbackForm.svelte";
+</script>
+
+<main class="container">
+  <FeedbackForm/>
+  <FeedbackStats/>
+  <FeedbackList/>
+</main>
